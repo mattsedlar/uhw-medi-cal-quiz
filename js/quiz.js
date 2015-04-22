b8912c1bdb65c6e1c6fbaa65872e4f7d911d2080
mattsedlar <sedlarm@gmail.com>
2015-04-22 12:56:10 -0700

    Added updated quiz questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,15 +1,14 @@
 var data = {
-  question1: {
+    question1: {
         question: "Who is covered by Medi-Cal?",
         choice1: "The disabled",
         choice2: "Children in poverty",
         choice3: "Seniors between 55 and 64 years old",
-        choice4: "The unemployed",
-        choice5: "All of the above",
-        answer: "5",
-        answernote: "Medi-Cal covers people living in poverty, the disabled, and children."
+        choice4: "All of the above",
+        answer: "4",
+        answernote: "Medi-Cal covers seniors not yet eligible for Medicare, people living in poverty who are employed or unemployed, the disabled, and children."
     },
-  question2: {
+    question2: {
         question: "The majority of Medi-Cal beneficiaries are:",
         choice1: "Adults",
         choice2: "Children",
@@ -17,5 +16,76 @@ var data = {
         choice4: "The disabled",
         answer: "2",
         answernote: "The majority of Medi-Cal beneficiaries are children (54%). Seniors are 12% of beneficiaries, adults are 34%."
+    },
+    question3: {
+        question: "How easy is it for a Medi-Cal patient to get a doctor’s appointment, compared to someone on private health insurance?",
+        choice1: "Easier",
+        choice2: "About the same",
+        choice3: "Harder",
+        answer: "3",
+        answernote: "Adults on Medi-Cal are twice as likely to have difficulties getting a doctor’s appointment, compared to adults with private insurance."
+    },
+    question4: {
+        question: "Medi-Cal covers what percentage of births in California?",
+        choice1: "12%",
+        choice2: "25%",
+        choice3: "50%",
+        answer: "3",
+        answernote: "Nearly half the births in California are covered through Medi-Cal."
+    },
+    question5: {
+        question: "Are most Medi-Cal beneficiaries male or female?",
+        choice1: "Male",
+        choice2: "Female",
+        answer: "2",
+        answernote: "56% of Medi-Cal beneficiaries are female."
+    },
+    question6: {
+        question: "How much of care for nursing home residents is covered by Medi-Cal?",
+        choice1: "1/3",
+        choice2: "1/2",
+        choice3: "2/3",
+        answer: "3",
+        answernote: "Medi-Cal pays for services for two-thirds of nursing home residents."
+    },
+    question7: {
+        question: "How many California children get health coverage through Medi-Cal?",
+        choice1: "One in ten",
+        choice2: "One in five",
+        choice3: "One in three",
+        answer: "3",
+        answernote: "One in three California children is covered through Medi-Cal."
+    },
+    question8: {
+        question: "California hospitals receive _____ of their net funding from Medi-Cal.",
+        choice1: "10%",
+        choice2: "20%",
+        choice3: "40%",
+        answer: "2",
+        answernote: "Hospitals rely on Medi-Cal for 20% of their annual net funding."
+    },
+    question9: {
+        question: "California doctors lose $_____ per year treating Medi-Cal patients.",
+        choice1: "$5 million",
+        choice2: "$1 billion",
+        choice3: "$5 billion",
+        answer: "3",
+        answernote: "Because Medi-Cal payment rates are so low, doctors in California lose $5.2 billion every year in treating Medi-Cal patients."
+    },
+    question10: {
+        question: "Medi-Cal pays healthcare providers _____ the cost of providing care.",
+        choice1: "100%",
+        choice2: "75%",
+        choice3: "50%",
+        answer: "3",
+        answernote: "On average, Medi-Cal pays healthcare providers like doctors, hospitals, and clinics only 50% of the cost of the care they provide to Medi-Cal patients."
+    },
+    question11: {
+        question: "How many Californians live in an area with a shortage of primary care doctors?",
+        choice1: "5 million",
+        choice2: "11 million",
+        choice3: "18 million",
+        answer: "3",
+        answernote: "Almost 18 million Californians live in an area with a shortage of primary care doctors."
     }
 };
