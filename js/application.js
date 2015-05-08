821b6d5cd2fba33a767f763738786d47c8f7f64a
mattsedlar <sedlarm@gmail.com>
2015-05-08 15:14:15 -0700

    Handling click events using bind, unbind

--- js/application.js
+++ js/application.js
@@ -2,46 +2,9 @@ var score = 0,
     counter = 1,
     ansMessage;
 
-function nextQuestion(model){
-
-  if(counter > 11) {
-
-    $("#question-container").css("display","none");
-    $("#score-container h2").html("You got " + score + " correct");
-    $("#score-container").css("display","block");
-
-  }
-
-
-  else {
-
-  var questionView = new app.QuestionView({model:model}),
-      l = 1;
-    questionView.render();
-		for (x in model.attributes) {
-			if(model.attributes.hasOwnProperty(x)){
-        if(x.indexOf("choice") > -1) {
-          $("#choice-container").append("<div class='choice' data-answer='no' id='choice"
-                                        + l
-                                        + "'><div>"
-                                        + model.attributes[x]
-                                        + "</div></div>");
-          l++;
-  			}
-			}
-		}
-   $("#choice" + model.attributes.answer).attr("data-answer","yes");
-   $("#count").html(counter + " ");
-
-   $("#next").attr("onclick","nextQuestion(eval('question' + counter))");
-
-   counter++;
-
-  $(".cover").slideUp();
-    $("#next").css({opacity:0.0});
-
-  $(".choice").click(function() {
-    if($(this).attr("data-answer") == "yes") {
+// this will be called in bind() and removed unbind()
+var clickHandler = function () {
+     if($(this).attr("data-answer") == "yes") {
       $(this).animate({ marginLeft:'0px'}, function(){
       if ( $(window).width() < 768 ) {
         $("#answer-container p").prepend("Correct! ").css({"display":"block","height":"auto","color":"black"}).fadeIn();
@@ -70,8 +33,49 @@ function nextQuestion(model){
     }
 
     $("#next").fadeTo("slow",1.0);
+    $(".choice").unbind("click", clickHandler);
+}
 
-  });
+function nextQuestion(model){
+
+  if(counter > 11) {
+
+    $("#question-container").css("display","none");
+    $("#score-container h2").html("You got " + score + " correct");
+    $("#score-container").css("display","block");
+
+  }
+
+  else {
+
+  var questionView = new app.QuestionView({model:model}),
+      l = 1;
+    questionView.render();
+		for (x in model.attributes) {
+			if(model.attributes.hasOwnProperty(x)){
+        if(x.indexOf("choice") > -1) {
+          $("#choice-container").append("<div class='choice' data-answer='no' id='choice"
+                                        + l
+                                        + "'><div>"
+                                        + model.attributes[x]
+                                        + "</div></div>");
+          l++;
+  			}
+			}
+		}
+
+
+   $("#choice" + model.attributes.answer).attr("data-answer","yes");
+   $("#count").html(counter + " ");
+
+   $("#next").attr("onclick","nextQuestion(eval('question' + counter))");
+
+   counter++;
+
+  $(".cover").slideUp();
+    $("#next").css({opacity:0.0});
+
+  $(".choice").bind("click", clickHandler);
 
   }
 }
